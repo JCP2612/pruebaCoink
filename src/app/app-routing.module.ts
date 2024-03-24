d42ee350dc59cfaa5b2a9d547238ebe3b18b432c
JCP2612 <jurycediel@gmail.com>
2024-03-24 16:28:43 -0500

    second commit step1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'step1',
+    loadChildren: () => import('./components/form/step1/step1.module').then( m => m.Step1PageModule)
+  },
 ];
 
 @NgModule({
